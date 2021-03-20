af6154dcfbe74af5e63063077f6b517c3bf46be9
Even Stensberg <evenstensberg@gmail.com>
2021-03-20 21:01:56 +0100

    git init

---
+++ utils/src/run-prettier.ts
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import prettier from 'prettier';
+import logger from 'webpack-cli/lib/utils/logger';
+
+/**
+ *
+ * Runs prettier and later prints the output configuration
+ *
+ * @param {String} outputPath - Path to write the config to
+ * @param {Node} source - AST to write at the given path
+ * @returns {Void} Writes a file at given location
+ */
+
+export function runPrettier(outputPath: string, source: string): void {
+    let prettySource: string = source;
+    try {
+        prettySource = prettier.format(source, {
+            filepath: outputPath,
+            parser: 'babel',
+            singleQuote: true,
+            tabWidth: 1,
+            useTabs: true,
+        });
+    } catch (err) {
+        logger.warn(`\nWARNING: Could not apply prettier to ${outputPath} due to validation error, but the file has been created`);
+        prettySource = source;
+    }
+
+    return fs.writeFileSync(outputPath, prettySource, 'utf8');
+}
